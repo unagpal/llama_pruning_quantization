355dbd69e03b4cf591e0e82b5c738e44ffa67575
Udai Nagpal <ugnagpal@gmail.com>
2025-05-10 22:19:15 -0400

    Update cache_llama_models.py

--- cache_llama_models.py
+++ cache_llama_models.py
@@ -7,6 +7,7 @@ import logging
 MODEL = "meta-llama/Llama-3.2-3B"
 MODEL_PATH = "./llama_models/"
 
+# Save bf16 Llama model to local filesystem
 def cache_model_bf16():
     model_dir = f"{MODEL_PATH}bf16"
     tokenizer = AutoTokenizer.from_pretrained(MODEL, use_fast=True,
@@ -17,13 +18,13 @@ def cache_model_bf16():
     os.makedirs(model_dir)
     tokenizer.save_pretrained(model_dir)
     model.save_pretrained(model_dir)
-    
+
+# Save nf4 Llama model to local filesystem
 def cache_model_nf4():
     model_dir = f"{MODEL_PATH}nf4"
     tokenizer = AutoTokenizer.from_pretrained(MODEL, use_fast=True,
                                               trust_remote_code=True,
                                                use_auth_token=True)
-    # nf4 quantization: https://huggingface.co/docs/transformers/en/quantization/bitsandbytes
     q_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_quant_type="nf4",
                                                     bnb_4bit_use_double_quant=True)
     model = LlamaForCausalLM.from_pretrained(MODEL, device_map="auto",
@@ -32,6 +33,7 @@ def cache_model_nf4():
     tokenizer.save_pretrained(model_dir)
     model.save_pretrained(model_dir)
 
+# Save int8 Llama model to local filesystem
 def cache_model_int8():
     model_dir = f"{MODEL_PATH}int8"
     tokenizer = AutoTokenizer.from_pretrained(MODEL, use_fast=True,
