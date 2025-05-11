856c9e13926a490728ec2a3784150fa69ce1196d
Udai Nagpal <ugnagpal@gmail.com>
2025-05-10 22:40:04 -0400

    Update comments

--- llama.py
+++ llama.py
@@ -10,16 +10,17 @@ MODEL = "meta-llama/Llama-3.2-3B"
 MODEL_PATH = "./llama_models/"
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
+# Load Llama nf4 quantized model from local file system
 def load_model_nf4_bitsandbytes():
     model_dir = f"{MODEL_PATH}nf4"
     tokenizer = AutoTokenizer.from_pretrained(model_dir, use_fast=True, local_files_only=True)
-    # nf4 quantization: https://huggingface.co/docs/transformers/en/quantization/bitsandbytes
     q_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_quant_type="nf4",
                                                     bnb_4bit_use_double_quant=True)
     model = LlamaForCausalLM.from_pretrained(model_dir, device_map="auto",
         quantization_config = q_config, local_files_only=True)
     return model, tokenizer
 
+# Load Llama int8 quantized model from local file system
 def load_model_int8_bitsandbytes():
     model_dir = f"{MODEL_PATH}int8"
     tokenizer = AutoTokenizer.from_pretrained(model_dir, use_fast=True, local_files_only=True)
@@ -28,6 +29,7 @@ def load_model_int8_bitsandbytes():
                                              local_files_only=True)
     return model, tokenizer
 
+#Load Llama bf16 model from local file system
 def load_model_bf16():
     model_dir = f"{MODEL_PATH}bf16"
     tokenizer = AutoTokenizer.from_pretrained(model_dir, use_fast=True, local_files_only=True)
